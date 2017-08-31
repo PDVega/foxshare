b9f819260376ab50731f3514ee8ddb922489ba56
msrabbani <msrabbanii@gmail.com>
2017-08-31 10:33:27 +0700

    cloud db selesai

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const express = require('express'),
 
 require('dotenv').config()
 const index = require('./routes/index');
-const signup = require('./routes/signup');
+const signup  = require('./routes/signup');
 const signin = require('./routes/signin');
 
 
-mongoose.connect('mongodb://localhost/foxshare');
+mongoose.connect('mongodb://msrabbani:lT4melzO8DbkotsO@foxshare-shard-00-00-9yq1c.mongodb.net:27017,foxshare-shard-00-01-9yq1c.mongodb.net:27017,foxshare-shard-00-02-9yq1c.mongodb.net:27017/foxshare?ssl=true&replicaSet=foxshare-shard-0&authSource=admin');
+
 
 // app.use(logger('dev'));
 app.use(bodyParser.json());
