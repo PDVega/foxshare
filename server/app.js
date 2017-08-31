67e37fc79d3e0a67c1393641d64ceb8197b13594
pdvega <pdvegaa@gmail.com>
2017-08-31 09:45:16 +0700

    upload file to s3 done

--- server/app.js
+++ server/app.js
@@ -1,17 +1,24 @@
 const express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
-    mongoose = require('mongoose'),
-    cors = require('cors');
+    bodyParser = require('body-parser');
+    // mongoose = require('mongoose'),
+    // cors = require('cors');
+// var S3 = require('aws-sdk/clients/s3');
     
-require('dotenv').config()
+// require('dotenv').config()
     
 const index = require('./routes/index');
 
 // app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+// app.use(cors());
+// app.use(function(req, res, next){
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", 'DELETE, PUT, POST');
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 
 app.use('/', index);
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('Alive');
-});
+var aws = require('aws-sdk');
+var router = express.Router();
+var multerS3 = require('multer-s3');
+var multer = require('multer');
+
+aws.config.loadFromPath('./config.json');
+aws.config.update({
+    signatureVersion: 'v4'
+})
+
+var s0 = new aws.S3({});
+
+var upload = multer({
+    storage: multerS3({
+        s3: s0,
+        bucket: 'foxshare', //your bucket name here 
+        acl: 'public-read',
+
+        //name of file
+        metadata: function(req, file, cb) {
+            cb(null, {
+                fieldname: file.fieldname
+            });
+        },
+        //unique description/date for the file
+        key: function(req, file, cb) {
+            cb(null, Date.now() + file.originalname)
+        }
+    })
+})
+
+router.get('/profile/upload', function(req, res, next) {
+    res.send('Form File for Submit');
+})
+
+router.get('/public/images', function(req, res, next) {
+    res.send('File image');
+})
+
+router.post('/profile/upload', upload.any(), function(req, res, next) {
+    res.send(req.files);
+    console.log(req.files)
+})
 
 module.exports = router;
